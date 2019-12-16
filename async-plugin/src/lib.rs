194a06f273ec994461c16b85bee3568e94dd7c06
Gary Hai <gary@neunit.com>
2019-12-16 13:16:59 +0800

    add features to separate runtimes

--- async-plugin/src/lib.rs
+++ async-plugin/src/lib.rs
@@ -1,33 +1,31 @@
-use std::{
-    future::Future,
-    pin::Pin,
-};
 use plugin_common::*;
-use async_std::task::spawn;
+use std::{future::Future, pin::Pin};
 
 #[no_mangle]
-pub extern "Rust" fn new_service() -> Pin<Box<dyn Future<Output = usize> + Send + Sync>> {
+pub fn new_service() -> Pin<Box<dyn Future<Output = usize> + Send + Sync>> {
     Box::pin(test_a())
 }
 
 #[no_mangle]
-pub static lib_declaration: LibDeclaration =
-    LibDeclaration {
-        rustc_version: "abc",
-        ns_version: "123",
-        register: new_service,
-    };
+pub static lib_declaration: LibDeclaration = LibDeclaration {
+    rustc_version: "abc",
+    ns_version: "123",
+    register: new_service,
+};
 
+#[cfg(feature = "rt-async-std")]
 async fn test_a() -> usize {
-    spawn(test_b());
-    10
-} 
-    
+    async_std::task::spawn(test_b()).await
+}
+
+#[cfg(feature = "rt-tokio")]
+async fn test_a() -> usize {
+    tokio::task::spawn(test_b()).await.unwrap()
+}
 
 async fn test_b() -> usize {
     10
-} 
-
+}
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use libloading::Library;
 use plugin_common::*;
 
+#[cfg(feature = "rt-async-std")]
 #[async_std::main]
 async fn main() {
+    main_test().await;
+}
+
+#[cfg(feature = "rt-tokio")]
+#[tokio::main]
+async fn main() {
+    main_test().await;
+}
+
+async fn main_test() {
     let libname = "/Users/gary/tmp/rust/debug/libasync_plugin.dylib";
     let library = Library::new(libname).unwrap();
     let decl = unsafe {
